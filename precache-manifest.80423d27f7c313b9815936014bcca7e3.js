62d1eb5a83cb68d5033a44b8e0e72cb504602c16
victoria <larikon@hotmail.com>
2020-10-20 10:54:18 +0300

    Updates

---
+++ precache-manifest.80423d27f7c313b9815936014bcca7e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc418011909c754e23469ba4bf9998bc",
+    "url": "/world-weather/index.html"
+  },
+  {
+    "revision": "faa957b3232c34da2156",
+    "url": "/world-weather/static/css/main.2c8b3b80.chunk.css"
+  },
+  {
+    "revision": "5dafd28fbec4ce42e1b4",
+    "url": "/world-weather/static/js/2.6d7008a0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/world-weather/static/js/2.6d7008a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa957b3232c34da2156",
+    "url": "/world-weather/static/js/main.261b9819.chunk.js"
+  },
+  {
+    "revision": "edb7722c260066f22f6d",
+    "url": "/world-weather/static/js/runtime-main.92cd33e8.js"
+  },
+  {
+    "revision": "7e73c770a6df1126579c523661a8f6e1",
+    "url": "/world-weather/static/media/cold.7e73c770.jpg"
+  },
+  {
+    "revision": "4b4742e5dcbfb890b90abcfe6e05adb0",
+    "url": "/world-weather/static/media/hot.4b4742e5.jpg"
+  },
+  {
+    "revision": "b7d3f7ccbaa5c1e7d8053ebf52372c90",
+    "url": "/world-weather/static/media/warm.b7d3f7cc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2057929cff0997f39b7a49be94a31df.js"
+  "/world-weather/precache-manifest.80423d27f7c313b9815936014bcca7e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/world-weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
